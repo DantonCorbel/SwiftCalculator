43b169a39479e3da4a10f502f036914ff43dc6d7
DantonCorbel <30301067+DantonCorbel@users.noreply.github.com>
2019-04-11 11:45:46 +1200

    Update Calculate.swift

--- Calculator/Calculate.swift
+++ Calculator/Calculate.swift
@@ -10,32 +10,36 @@ import Foundation
 
 struct Numbers {
     var display: String
-    var storeNumber1: Int
-    var storeNumber2: Int
-    var storeNumber3: Int
+    var storeNumber1: Double
+    var storeNumber2: Double
+    var storeNumber3: Double
     
     mutating func displayString(_ number: String) {
         display += "\(number)"
-        storeNumber3 = Int(display)!
+        storeNumber3 = Double(display)!
         }
     
     mutating func storeFirstNum(_ display: String) {
         if !display.isEmpty {
-        storeNumber1 = Int(display)!
+        storeNumber1 = Double(display)!
         }
     }
     
     mutating func storeSecondNum(_ display: String) {
-        storeNumber2 = Int(display)!
-    }
+        storeNumber2 = Double(display)!    }
     
-    mutating func displayAnswer(_ storeNumber1: Int) {
+    mutating func displayAnswer(_ storeNumber1: Double) {
         display = String(storeNumber1)
     }
     
     mutating func changeSign(_ display: String) {
         storeNumber3 -= (storeNumber3 * 2)
         self.display = String(storeNumber3)
+    }
+    
+    mutating func percent(_ storeNumber3: Double) {
+        self.storeNumber3 = storeNumber3 / 100
+        display = String(self.storeNumber3)
     }
 }
 
